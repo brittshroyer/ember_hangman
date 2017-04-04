efe93ddda317a57cbaf5a02ec68e5ee7720336d8
brittshroyer <britt.shroyer@gmail.com>
2017-04-04 10:52:02 -0500

    +answer template

--- app/controllers/setanswer.js
+++ app/controllers/setanswer.js
@@ -1,5 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  //no need for name within parentheses if its the same
   signin: Ember.inject.controller(),
+  topic: '',
+  answer: ''
 });
